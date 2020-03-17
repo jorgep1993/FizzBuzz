893b3f6d67b4340e6c6948afbfeef741b9a8623c
jorgep <jorgep@DESKTOP-9O7VVRQ.localdomain>
2020-03-17 14:07:36 +0100

    C# solution to FizzBuzz done. Test class still needs more work

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FizzBuzzService fizzBuzzService = new FizzBuzzService();
+            foreach (var result in fizzBuzzService.GetFizzBuzz(100))
+            {
+                Console.WriteLine(result);               
+            }
+        }
+    }
+
+    class FizzBuzzTest
+    {
+        
+    }
+
+    class FizzBuzzService
+    {
+        public string[] GetFizzBuzz(int n)
+        {
+            string s;
+            string[] res = new string[n];
+            for (int i = 1; i <= n; i++)
+            {
+                s = "";
+                if (i % 3 == 0)
+                {
+                    s += "Fizz";
+                }
+                
+                if (i % 5 == 0)
+                {
+                    s += "Buzz";
+                }
+
+                if (s.Length == 0)
+                {
+                    s += i;
+                }
+
+                res[i - 1] = s;
+            }
+
+            return res;
+        }
+    }
+}
